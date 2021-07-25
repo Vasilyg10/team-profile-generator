d8d6ce6c3f4e12276781a73943445c96d56909ef
Vasilyg10 <zvgianna@gmail.com>
2021-07-25 13:06:49 -0500

    added Dependencies to index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
-
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 const createdEmployees = [];
 
