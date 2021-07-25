9fd46117fb745b2c7f31ae49a2860141240e5a93
Vasilyg10 <zvgianna@gmail.com>
2021-07-25 12:34:22 -0500

    completed Employee.test.js file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,22 @@
 const { test, expect } = require('@jest/globals')
 const Employee = require('../lib/employee')
 
-test('getName returns the right name', function(){
-    const testEmployee = new Employee('Frank', 5, 'frank@frank.com')
-    expect(testEmployee.getName()).toBe('Frank')
-})
+test('getName returns the right name', function() {
+    const testEmployee = new Employee('Frank', 5, 'frank@frank.com', 'Employee')
+    expect(testEmployee.getName()).toBe('Frank');
+});
+
+test('getId returns the right id', function() {
+    const testEmployee = new Employee('Frank', 5, 'frank@frank.com', 'Employee')
+    expect(testEmployee.getId()).toBe(5);
+});
+
+test('getEmail returns the right email', function() {
+    const testEmployee = new Employee('Frank', 5, 'frank@frank.com', 'Employee')
+    expect(testEmployee.getEmail()).toBe('frank@frank.com');
+});
+
+test('getRole returns the right role', function() {
+    const testEmployee = new Employee('Frank', 5, 'frank@frank.com', 'Employee')
+    expect(testEmployee.getRole()).toBe('Employee');
+});
